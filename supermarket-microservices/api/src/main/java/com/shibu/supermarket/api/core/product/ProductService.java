b8e96dd510169a93ac959099f61c3b7baa32c3c9
ShibuKuppelanLearning <shibu.kuppelan.learning@gmail.com>
2021-08-17 05:59:51 +0300

    git docker,docker-compose changes

--- supermarket-microservices/api/src/main/java/com/shibu/supermarket/api/core/product/ProductService.java
+++ supermarket-microservices/api/src/main/java/com/shibu/supermarket/api/core/product/ProductService.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductService {
+
 	@GetMapping(value= "/product/{productId}",produces = "application/json")
 	public Product getProduct(@PathVariable int productId);
+
 }
--- supermarket-microservices/microservices/product-service/src/test/java/com/shibu/microservices/core/product/ProductServiceApplicationTests.java
+++ supermarket-microservices/microservices/product-service/src/test/java/com/shibu/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.shibu.microservices.core.product;
 
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final int PRODUCT_ID_OK = 1;
 
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void getProductById() {
+		client.get().uri(StringUtils.join("/product-composite/", PRODUCT_ID_OK))
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectHeader()
+				.contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+				.jsonPath("$.recommendations.length()").isEqualTo(1)
+				.jsonPath("$.reviews.length()").isEqualTo(1);
+		;
+	}
 }
